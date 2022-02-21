2a5bda9302e64f7b0b84eea93719572d8b77e76e
Joan Gimenez <joanf.gimenez@gmail.com>
2022-02-21 15:14:47 -0300

    [ADD] sponsors section

---
+++ src/containers/Sponsors.jsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { brands } from "../services/listOfBrands";
+import "@styles/sponsors.scss";
+
+const Sponsors = () => {
+  return (
+    <section className="sponsors">
+      <h2 className="sponsors__title">
+        Empresas que confían en nuestro trabajo
+      </h2>
+      <ul className="sponsors__list">
+        {brands.map(({ src, alt }) => (
+          <li className="sponsors__items">
+            <img src={src} alt={alt} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Sponsors;
