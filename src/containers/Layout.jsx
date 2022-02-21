b4d31705d924afbc83fe2a1981cb8d1822ac02a8
Joan Gimenez <joanf.gimenez@gmail.com>
2022-02-21 15:13:27 -0300

    [ADD] layout container

---
+++ src/containers/Layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "@containers/Header";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+};
+
+export default Layout;
