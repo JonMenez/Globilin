9c9f5f42b1f8e20e67a21f4ac74502a4e2691a29
Joan Gimenez <joanf.gimenez@gmail.com>
2022-02-21 15:15:00 -0300

    [ADD] list of brands

---
+++ src/services/listOfBrands.js
@@ -0,0 +1,19 @@
+import coca from "@icons/coca.svg";
+import nestle from "@icons/nestle.svg";
+import conaprole from "@icons/conaprole.svg";
+import anda from "@icons/anda.svg";
+import tata from "@icons/tata.svg";
+import antel from "@icons/antel.svg";
+import bse from "@icons/bse.svg";
+import banco from "@icons/banco.svg";
+
+export const brands = [
+  { src: coca, alt: "coca-cola coke company uruguay" },
+  { src: nestle, alt: "nestle cafe nestcafe company uruguay" },
+  { src: conaprole, alt: "conaprole uruguay" },
+  { src: anda, alt: "anda uruguay" },
+  { src: tata, alt: "tata.com.uy tata uruguay" },
+  { src: antel, alt: "antel telecomunicaciones uruguay" },
+  { src: bse, alt: "bse uruguay" },
+  { src: banco, alt: "banco republica uruguay" },
+];
