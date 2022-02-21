fa8bdf96abaf2f3d020dccae9c2ecfd16f8c6a8e
Joan Gimenez <joanf.gimenez@gmail.com>
2022-02-21 15:12:22 -0300

    [ADD] home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Hero from "@containers/Hero";
+import Sponsors from "@containers/Sponsors";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Sponsors />
+    </>
+  );
+};
+
+export default Home;
