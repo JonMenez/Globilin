33493b03e1a20b8ab4faab313f36f3b3a5f4e7b6
Joan Gimenez <joanf.gimenez@gmail.com>
2022-02-22 15:01:32 -0300

    [UPDATE] hero image

--- src/containers/Hero.jsx
+++ src/containers/Hero.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Button from "@components/Button";
 import { useNavigate } from "react-router-dom";
+import hero from "@images/hero.png";
 import "@styles/hero.scss";
 
 const Hero = () => {
@@ -11,14 +12,23 @@ const Hero = () => {
   return (
     <main className="hero">
       <section className="hero__container">
-        <h1 className="hero__title">
-          Porque en todos los momentos lindos siempre hay globos
-        </h1>
-        <p className="hero__body">
-          Y nosotros tenemos todo lo que necesitás para que tu momento sea aún
-          más especial
-        </p>
-        <Button style="hero__button" textContent="Contacto" click={toContact} />
+        <div className="hero__text">
+          <h1 className="hero__title">
+            Porque en todos los momentos lindos siempre hay globos
+          </h1>
+          <p className="hero__body">
+            Y nosotros tenemos todo lo que necesitás para que tu momento sea aún
+            más especial
+          </p>
+          <Button
+            style="hero__button"
+            textContent="Contacto"
+            click={toContact}
+          />
+        </div>
+        <div className="hero__img">
+          <img src={hero} alt="" />
+        </div>
       </section>
     </main>
   );
